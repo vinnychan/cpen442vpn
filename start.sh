cd6ffbfa14a2a01b13039a1107586fd378d75749
yves-chan <chanyves@gmail.com>
2016-10-19 09:39:22 -0700

    updated start script

--- start.sh
+++ start.sh
@@ -1,2 +1,18 @@
 #!/bin/sh
+is_installed(){
+  echo "Checking if $1 is installed."
+  local status="$1 is installed."
+  type $1>/dev/null 2>&1 ||
+    { local status="$1 is not installed, Aborting"; exit 1;}
+  echo "$status";
+  echo "---"
+}
+
+is_installed go
+is_installed node
+is_installed electron
+
+echo "All pre-requisites are installed."
+echo "Starting VPN program..."
+
 electron gui/main.js
